25fbad425d8c434175d7d7ff983078ab9b290503
Alexander Biryukov <74318440+AlexFromNorth@users.noreply.github.com>
2024-05-16 23:36:11 +0300

    version 1.0

--- js/main.js
+++ js/main.js
@@ -4,49 +4,49 @@ const ctx = canvas.getContext("2d");
 const image = new Image();
 image.src = "png.png";
 
-canvas.width = image.width
-canvas.height = image.height 
+canvas.width = image.width;
+canvas.height = image.height;
 
-const imageArr = []
+const imageArr = [];
 
 image.addEventListener("load", () => {
   ctx.drawImage(image, 0, 0, image.width, image.height);
 
   const imageData = ctx.getImageData(0, 0, image.width, image.height);
 
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    for(let i = 0; i < imageData.data.length; i+=4){
-      if(imageData.data[i+3] != 0){
-        imageArr.push({
-          r: imageData.data[i],
-          g: imageData.data[i+1],
-          b: imageData.data[i+2],
-          a: imageData.data[i+3],
-          x: Math.floor((i/4))%image.width  ,
-          y: Math.floor(Math.floor((i/4))/image.width),
-          vX: random(),
-          vY: random(),
-        })
-      }
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    if (imageData.data[i + 3] != 0) {
+      imageArr.push({
+        r: imageData.data[i],
+        g: imageData.data[i + 1],
+        b: imageData.data[i + 2],
+        a: imageData.data[i + 3],
+        x: Math.floor(i / 4) % image.width,
+        y: Math.floor(Math.floor(i / 4) / image.width),
+        vX: random(),
+        vY: random(),
+      });
     }
-    (function drawCanvas(){
-      ctx.clearRect(0, 0, canvas.width, canvas.height)
-      
-      imageArr.forEach((el)=>{
-        ctx.fillStyle = `rgba(${el.r},${el.g},${el.b},${el.a})`;
-        ctx.fillRect(el.x, el.y, 1, 1)
+  }
+  (function drawCanvas() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-        el.x += el.vX
-        el.y += el.vY
+    imageArr.forEach((el) => {
+      ctx.fillStyle = `rgba(${el.r},${el.g},${el.b},${el.a})`;
+      ctx.fillRect(el.x, el.y, 1, 1);
 
-        if(el.x >= canvas.width -1 || el.x <= 0){
-          el.vX = -el.vX
-        }
-        if(el.y >= canvas.height -1 || el.y <= 0){
-          el.vY = -el.vY
-        }
-      })
-      requestAnimationFrame(drawCanvas)
-    }())
+      el.x += el.vX;
+      el.y += el.vY;
+
+      if (el.x >= canvas.width - 1 || el.x <= 0) {
+        el.vX = -el.vX;
+      }
+      if (el.y >= canvas.height - 1 || el.y <= 0) {
+        el.vY = -el.vY;
+      }
+    });
+    requestAnimationFrame(drawCanvas);
+  })();
 });
--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 const randomScale = 0.5
 
 function random(){
-    return Math.round(Math.random()) == 0 ? 1 * Math.random()*randomScale : -1 * Math.random()*randomScale;
+    return (Math.round(Math.random()) == 0 ? 1 : -1) * Math.random()*randomScale;
 }
